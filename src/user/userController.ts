9e7664a3009596ee935d985b3019814be8ceffac
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2024-07-16 10:17:15 +0530

    Add register user Validation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,7 +1,16 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    res.json({ message: "user created successfully" });
+    const { name, email, password } = req.body;
+
+    //validation
+    if (!name || !password || !email) {
+        const error = createHttpError(400, "All fields are required");
+        return next(error);
+    }
+
+    return res.json({ message: "User created succesfully" });
 };
 
 export { createUser };
