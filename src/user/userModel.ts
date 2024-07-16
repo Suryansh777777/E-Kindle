fef3969f99c48d7fb43e9a8fd8de4bc3ea58c3e1
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2024-07-16 12:05:37 +0530

    Add usermodel

---
+++ src/user/userModel.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { User } from "./userTypes";
+
+const userSchema = new mongoose.Schema<User>(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+//users collections
+export default mongoose.model<User>("User", userSchema);
